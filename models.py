c5a842e9b19b67fc3b8d4d1dcf023c08db7d0a50
aulorbe <aulorbe@gmail.com>
2018-11-06 15:08:40 -0500

    finished non-querying part of lab

--- models.py
+++ models.py
@@ -2,16 +2,65 @@ from sqlalchemy import *
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
 
+from sqlalchemy import create_engine
+
 Base = declarative_base()
 
+## BELOW = CREATING MY TABLES
+class City(Base):
+    __tablename__ = 'city'
+    id = Column(Integer, primary_key=True)
+    name = Column(Text)
+    state = Column(Text)
+    city_ = relationship('Teams')
 
-# write the Player, City, Sport and Team tables below
+
+class Sports(Base):
+    __tablename__ = 'sports'
+    id = Column(Integer, primary_key=True)
+    name = Column(Text)
+    sport_ = relationship('Teams')
 
 
+class Teams(Base):
+    __tablename__ = 'teams'
+    id = Column(Integer, primary_key=True)
+    name = Column(Text)
+
+    teams_ = relationship('Players')
+
+    city_id = Column(Integer, ForeignKey('city.id')) #this one
+    city = relationship(City, back_populates='city_')
+
+    sport_id = Column(Integer, ForeignKey('sports.id')) #this one
+    sport = relationship(Sports, back_populates='sport_')
 
 
+class Players(Base):
+    __tablename__ = 'players'
+    id = Column(Integer, primary_key=True)
+    name = Column(Text)
+    number = Column(Integer, default=None)
+    height = Column(Text)
+    weight = Column(Float)
+    age = Column(Text)
+    team_id = Column(Integer, ForeignKey('teams.id')) # this one
+    team = relationship(Teams, back_populates='teams_')
 
 
-
-engine = create_engine('sqlite:///sports.db')
-Base.metadata.create_all(engine)
+##### BELOW = INSTANTIATING MY OBJECTS
+# engine = create_engine('sqlite:///sports.db')
+# Base.metadata.create_all(engine)
+#
+#
+# Session = sessionmaker(bind=engine)
+# session = Session()
+#
+# nyc = City(name='New York City', state='New York')
+# la = City(name='Los Angeles', state='California' )
+#
+# bball = Sports(name='Basketball')
+# baseball = Sports(name='Baseball')
+#
+# session.add_all([la,nyc,bball,baseball])
+# session.commit()
--- seed.py
+++ seed.py
@@ -1,32 +1,76 @@
 from models import *
 from sqlalchemy import create_engine
 import pandas as pd
+from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('sqlite:///actors.db')
+engine = create_engine('sqlite:///sports.db')
+Base.metadata.create_all(engine)
 
 Session = sessionmaker(bind=engine)
 session = Session()
 
-# below we are reading the csv files to create the data we will need to create the players
-# pandas returns a DataFrame object from reading the CSV
-# we then tell the DataFrame object to turn each row into dictionaries
-# by giving to_dict the argument "orient='records'"
-# we are telling our DataFrame to make each row a dictionary using the column headers
-# as the keys for the key value pairs in each new dictionary
-# feel free to uncomment lines 18-21 to see each step of the process in your terminal
-# ____ example ______
-# la_dodgers0 = pd.read_csv('la_dodgers_baseball.csv')
-# la_dodgers1 = pd.read_csv('la_dodgers_baseball.csv').to_dict()
-# la_dodgers2 = pd.read_csv('la_dodgers_baseball.csv').to_dict(orient='records')
-# import pdb; pdb.set_trace()
-# __________________
+# CITY OBJECTS
+la = City(name='Los Angeles', state='CA')
+nyc = City(name='New York City', state='NY')
+session.add_all([la, nyc])
+session.commit()
+
+# SPORTS OBJECTS
+baseball = Sports(name='Baseball')
+bball = Sports(name='Basketball')
+session.add_all([baseball, bball])
+session.commit()
+
+# READING CSVS
 la_dodgers = pd.read_csv('la_dodgers_baseball.csv').to_dict(orient='records')
 la_lakers = pd.read_csv('la_lakers_basketball.csv').to_dict(orient='records')
 ny_yankees = pd.read_csv('ny_yankees_baseball.csv').to_dict(orient='records')
 ny_knicks = pd.read_csv('ny_knicks_basketball.csv').to_dict(orient='records')
 
+# FUNCTION TO CREATE PLAYERS
+def populate_teams(data):
+    player_list = []
+    for x in data:
+        P = Players(name=x.get('name'),age = x.get('age', None),number=x.get('number', None), height=x.get('height',None), weight=x.get('weight', None))
+        player_list.append(P)
+    return player_list
 
-# now that we have the data for each player
-# add and commit the players, teams, sports and cities below
-# we will need to probably write at least one function to iterate over our data and create the players
-# hint: it may be a good idea to creat the Teams, Cities, and Sports first 
+# SETTING VARIABLES TO OUTCOME OF ABOVE FUNCTION PER TEAM
+dodgers_players = populate_teams(la_dodgers)
+lakers_players = populate_teams(la_lakers)
+yankees_players = populate_teams(ny_yankees)
+knicks_players = populate_teams(ny_knicks)
+
+# TEAM OBJECTS
+dodgers = Teams(name='Dodgers', city=la, sport=baseball, teams_=dodgers_players)
+yankees = Teams(name='NY Yankees', sport=baseball, city=nyc, teams_=yankees_players)
+lakers = Teams(name='LA Lakers', sport=bball, city=la, teams_=lakers_players)
+knicks = Teams(name='NY Knicks', sport=bball, city=nyc, teams_=knicks_players)
+
+# ADDING & COMMITTING TEAM OBJECTS
+session.add_all([dodgers, lakers, yankees, knicks])
+session.commit()
+
+# dodgers = Teams(
+#     name = 'LA Dodgers',
+#     city_id = session.query(City).filter(City.name=='Los Angeles').first().id,
+#     sport_id = session.query(Sports).filter(Sports.name=='Baseball').first().id
+#     )
+#
+# lakers = Teams(
+#     name = 'LA Lakers',
+#     city_id = session.query(City).filter(City.name=='Los Angeles').first().id,
+#     sport_id = session.query(Sports).filter(Sports.name=='Basketball').first().id
+#     )
+#
+# knicks = Teams(
+#     name = 'NY Knicks',
+#     city_id = session.query(City).filter(City.name=='New York City').first().id,
+#     sport_id = session.query(Sports).filter(Sports.name=='Basketball').first().id
+#     )
+#
+# yankees = Teams(
+#     name = 'NY Yankees',
+#     city_id = session.query(City).filter(City.name=='New York City').first().id,
+#     sport_id = session.query(Sports).filter(Sports.name=='Baseball').first().id
+#     )
